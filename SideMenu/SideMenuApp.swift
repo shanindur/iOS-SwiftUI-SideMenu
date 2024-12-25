bb69e316aad43585dd6870c3a34025f392f48331
shanindurajapaksha <shanindu.rajapaksha91@gmail.com>
2024-12-25 15:28:56 -0500

    Initial Commit

---
+++ SideMenu/SideMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  SideMenuApp.swift
+//  SideMenu
+//
+//  Created by Shanindu Rajapaksha on 2024-12-25.
+//
+
+import SwiftUI
+
+@main
+struct SideMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SideMenuTests/SideMenuTests.swift
@@ -0,0 +1,17 @@
+//
+//  SideMenuTests.swift
+//  SideMenuTests
+//
+//  Created by Shanindu Rajapaksha on 2024-12-25.
+//
+
+import Testing
+@testable import SideMenu
+
+struct SideMenuTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
